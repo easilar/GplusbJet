4ada28381097172739cb867c2e9b5db321bcbf94
Metin Yalvac <myalvac@lxplus.cern.ch>
2021-01-13 00:59:02 +0100

    getData workouts

--- submit_eff.sh
+++
@@ -1,3 +0,0 @@
-python /afs/cern.ch/work/e/ecasilar/GplusbJets/plot_efficiency.py --trig=0
-python /afs/cern.ch/work/e/ecasilar/GplusbJets/plot_efficiency.py --trig=1
-python /afs/cern.ch/work/e/ecasilar/GplusbJets/plot_efficiency.py --trig=2
