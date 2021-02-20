2b48af0486137acbf30e76a733ec1f0fbac5aca8
ecasilar <ece.asilar@cern.ch>
2021-02-20 20:09:00 +0100

    no print

--- prepare_submit.py
+++ prepare_submit.py
@@ -16,14 +16,14 @@ sdict = sample_dic[2016][stype][sname]
 for ci,bin_name in enumerate(sdict.keys()):
 	#if not "_NOExt" in bin_name: continue
 	cur_dir = sdict[bin_name]["dir"]
-	print(cur_dir)
+	#print(cur_dir)
 	flist = os.listdir(cur_dir)
 	if stype=="data":
-		print(os.environ["cern_box"]+"/data/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-1]+"/")
+		#print(os.environ["cern_box"]+"/data/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-1]+"/")
 		if not os.path.exists(os.environ["cern_box"]+"/data/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-1]+"/"):
 			os.makedirs(os.environ["cern_box"]+"/data/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-1]+"/")
 	else:
-		print(os.environ["cern_box"]+"/MC/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-2]+"/")
+		#print(os.environ["cern_box"]+"/MC/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-2]+"/")
 		if not os.path.exists(os.environ["cern_box"]+"/MC/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-2]+"/"):
 			os.makedirs(os.environ["cern_box"]+"/MC/"+sname+"/"+sdict[bin_name]["dir"].split("/")[-2]+"/")
 
